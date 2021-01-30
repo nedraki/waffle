f92b2e01e83a79b3660cf133f5089242020bf9f8
nedraki <66323603+nedraki@users.noreply.github.com>
2021-01-30 21:55:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.write('Hello world')
+
+st.header('Hello world')
+
+"""Testing importing script"""
+
+st.write("test html import")
+html_file = open("test.html", 'r', encoding='utf-8')
+source_code = html_file.read() 
+print(source_code)
+components.html(source_code)
